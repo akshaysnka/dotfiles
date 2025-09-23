d1a7c38d4cfc1fdcfd1b04d62df268a8e18bc018
Akshay Karthick Senthilnathan <akshaysnka@gmail.com>
2025-09-22 22:33:05 -0400

    remove newlines

--- script.sh
+++ script.sh
@@ -9,7 +9,3 @@ sudo apt install btop python3 idle
 wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -vo /etc/apt/keyrings/xanmod-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/xanmod-release.list
 sudo apt update && sudo apt install linux-xanmod-x64v3
-
-
-
-
