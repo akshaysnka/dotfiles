d5d601229cf80cdc9df50ebf35406754df67b212
Akshay Karthick Senthilnathan <akshaysnka@gmail.com>
2025-09-24 22:02:02 -0400

    add and enable tlp

--- script.sh
+++ script.sh
@@ -4,8 +4,10 @@ sudo apt upgrade
 
 curl https://dl.brave.com/install.sh | bash
 
-sudo apt install btop python3 idle
-
 wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -vo /etc/apt/keyrings/xanmod-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/xanmod-release.list
 sudo apt update && sudo apt install linux-xanmod-x64v3
+
+sudo apt install btop python3 idle tlp
+
+sudo systemctl enable --now tlp
