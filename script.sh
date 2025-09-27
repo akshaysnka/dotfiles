da2c50b36398c98b487db02b781152b9c269109d
Akshay <akshaysnka@gmail.com>
2025-09-26 21:26:30 -0400

    update script

--- script.sh
+++ script.sh
@@ -9,6 +9,15 @@ wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -vo /etc/apt/k
 echo "deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/xanmod-release.list
 sudo apt update && sudo apt install linux-xanmod-x64v3
 
-sudo apt install btop python3 idle tlp
+sudo apt install btop python3 idle tlp xournal easyeffects
+sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
 
+sudo adduser $USER libvirt
+sudo adduser $USER kvm
+
+sudo systemctl enable --now libvirtd
 sudo systemctl enable --now tlp
+
+echo
+echo
+echo 'REBOOT YOUR SYSTEM NOW!'
