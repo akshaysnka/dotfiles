d7ee65e4ed494a4a962bae0feb09c28bdadc0415
Akshay Senthilnathan <akshaysnka@gmail.com>
2025-09-26 22:14:27 -0400

    update

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -vo /etc/apt/k
 echo "deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/xanmod-release.list
 sudo apt update && sudo apt install linux-xanmod-x64v3
 
-sudo apt install btop python3 idle tlp xournal easyeffects
+sudo apt install btop python3 idle tlp xournal easyeffects pavucontrol
 sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
 
 sudo adduser $USER libvirt
